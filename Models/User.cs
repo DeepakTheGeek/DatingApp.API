930131700cc4d21528464fac686f90428d305fda
Deepak Das <Deepak.Das@brandmuscle.com>
2020-05-03 21:47:11 +0530

    creating the user model

---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [StringLength(25)]
+        public string UserName { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+    }
+}
