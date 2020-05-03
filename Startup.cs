8bda79d196716309411d8488d7cc0c314f17bf39
Deepak Das <Deepak.Das@brandmuscle.com>
2020-05-03 18:42:59 +0530

    Creating the first model and DataContext

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using DatingApp.API.Data;
 
 namespace DatingApp.API
 {
@@ -26,6 +28,7 @@ namespace DatingApp.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
