ee0961ed0a2b9260601f6cac432d07d4ccc1d549
Deepak Das <Deepak.Das@brandmuscle.com>
2020-05-03 19:34:04 +0530

    Retrieving data from database

---
+++ Controllers/ValueController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("Value")]
+    [ApiController]
+    public class ValueController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValueController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetValues()
+        {
+            var values = _context.Values.ToList();
+            return Ok(values);
+        }
+
+        [Route("{id}")]
+        [HttpGet]
+        public IActionResult GetValue(int id)
+        {
+            var value = _context.Values.FirstOrDefault(v => v.Id == id);
+            return Ok(value);
+        }
+    }
+}
