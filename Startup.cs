630cdb3190bfced864f00f2f0ff5c07dd57cce03
Deepak Das <Deepak.Das@brandmuscle.com>
2020-06-13 22:06:52 +0530

    .NET 3.0 requirements added

--- Startup.cs
+++ Startup.cs
@@ -34,7 +34,7 @@ namespace DatingApp.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson();
             services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddCors();
             services.AddScoped<IAuthRepository, AuthRepository>();
